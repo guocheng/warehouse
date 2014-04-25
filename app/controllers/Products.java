1b6ff95b615cbae94e0fd21411d6ec0ea06399a6
Cheng <UR@idea-PC>
2014-04-25 10:30:30 +0800

    Complete Chapter 3

--- app/controllers/Products.java
+++ app/controllers/Products.java
@@ -1,15 +1,16 @@
 package controllers;
 
+import play.data.Form;
 import java.util.Set;
-
 import models.Product;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.products.*;
 
-
 public class Products extends Controller
 {
+	private static final Form<Product> productForm = Form.form(Product.class);
+	
 	public static Result list()
 	{
 		Set<Product> products = Product.findAll();
@@ -18,16 +19,33 @@ public class Products extends Controller
 	
 	public static Result showBlank()
 	{
-		return TODO;
+		return ok(show.render(productForm));
 	}
 	
 	public static Result show(Long ean)
 	{
-		return TODO;
+		final Product product = Product.findByEan(ean);
+		if(product == null)
+		{
+			flash("error", String.format("Product %s does not exist.", ean));
+			return redirect(routes.Products.list());
+		}
+
+		Form<Product> filledForm = productForm.fill(product);
+		return ok(show.render(filledForm));
 	}
 	
 	public static Result save()
 	{
-		return TODO;
+		Form<Product> boundForm = productForm.bindFromRequest();
+		if(boundForm.hasErrors())
+		{
+			flash("error", "Please correct the form below.");
+			return badRequest(show.render(boundForm));
+		}
+		Product product = boundForm.get();
+		Product.add(product);
+		flash("success", String.format("Successfully added product %s", product));
+		return redirect(routes.Products.list());
 	}
 }
